8b0abe35694690d2cf3d8c4dcd69cf93e238ffe7
Yusuf Topkaya <yusuftopkaya@protonmail.com>
2026-06-26 20:38:52 +0300

    feat(experimental): durum makineli, akici marquee kayan menu etiketleri
    - 10 ms sistem tikinden beslenen hold-scroll-hold state machine eklendi.
    - Baslangicta ve sonda 1 sn bekleme; kayma hizi ~30 FPS (30 ms/piksel).
    - Her satir icin ayri state tutularak menu degisiminde animasyon sifirlaniyor.
    - Sol panel her karede temizlenerek ghosting engelleniyor.
    - Eski surekli dongulu marquee kaldirildi.

---
+++ scheduler.h
@@ -0,0 +1,26 @@
+/* Copyright 2023 Dual Tachyon
+ * https://github.com/DualTachyon
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *     Unless required by applicable law or agreed to in writing, software
+ *     distributed under the License is distributed on an "AS IS" BASIS,
+ *     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *     See the License for the specific language governing permissions and
+ *     limitations under the License.
+ */
+
+#ifndef SCHEDULER_H
+#define SCHEDULER_H
+
+#include <stdint.h>
+
+/* The SysTick interrupt increments a global 10 ms counter.
+ * This accessor lets UI code use it for smooth animation timing. */
+uint32_t SYS_Get10ms(void);
+
+#endif
